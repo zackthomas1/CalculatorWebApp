e1e7ff66cb5770c6bab552c28047c832119c3f7a
zackthomas1 <zackthomas13@gmail.com>
2021-12-03 13:36:20 -0800

    history div collapses under calculator when screen size smaller than large.

--- responsive.js
+++ responsive.js
@@ -4,13 +4,10 @@
 function resizeAppContainer(){
     if(window.innerWidth > 768) // medium and above
     {
-        document.querySelector("#app_container").classList.remove("w-75");
         document.querySelector("#app_container").classList.add("w-75");
-
     }
     else if(window.innerWidth >= 576 && window.innerWidth <= 768) // small
     {
-        document.querySelector("#app_container").classList.remove("w-75");
         document.querySelector("#app_container").classList.add("w-75");
     }
     else    // extra small
